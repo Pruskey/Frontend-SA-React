a82ad6d2d075059fe407f3280547091def4667dc
BRUNO DAL PRÁ <bruno_pra@estudante.sesisenai.org.br>
2025-09-15 09:45:54 -0300

    toma

---
+++ my-app/src/components/Header2.jsx
@@ -0,0 +1,19 @@
+import '../style/Header.css';
+import '../style/MenuOption.css';
+import '../style/Logo.css';
+
+function Header() {
+    return (
+        <>
+        <div classname="logo">logo</div>
+        <div className="header">
+        <div className="option">toma</div>
+        <div className="option">toma2</div>
+        <div className="option">toma3</div>
+        <div className="option">toma4</div>
+        </div>
+        </>
+    );
+}
+
+export default Header;
