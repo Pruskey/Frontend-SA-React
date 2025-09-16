905e8e30cdbe7eb98916dfc297f79083e1c97d64
BRUNO DAL PRÁ <bruno_pra@estudante.sesisenai.org.br>
2025-09-16 11:16:44 -0300

    header quase pronto

--- my-app/src/components/Header2.jsx
+++ my-app/src/components/Header2.jsx
@@ -2,22 +2,28 @@ import '../style/Header.css';
 import '../style/MenuOption.css';
 import '../style/Logo.css';
 import '../style/Configuracao.css'
-import '../style/Botao-Motorista.css'
+import '../style/MenuOption2.css'
 import tomaImg from '../assets/toma.png';
 
 function Header() {
     return (
         <>
-        <button className='motorista'>login motorista</button>
+        <button className='option2'>login motorista</button>
+
         <button className='configuracao'>
         <img src={tomaImg} alt ="toma" width="65" height="65"/>
         </button>
+
         <div className="logo">logo</div>
+
         <div className="header">
-        <button className="option">toma</button>
-        <button className="option">toma2</button>
-        <button className="option">toma3</button>
-        <button className="option">toma4</button>
+
+        <button className="option">HOME</button>
+
+        <button className="option">HORÁRIOS</button>
+
+        <button className="option">CONTATO</button>
+        
         </div>
         </>
     );
---
+++ my-app/src/components/Noticias.jsx
@@ -0,0 +1,21 @@
+import '../style/Header.css';
+import '../style/MenuOption.css';
+import '../style/Logo.css';
+import '../style/Configuracao.css'
+import '../style/MenuOption2.css'
+import tomaImg from '../assets/toma.png';
+
+function Noticias() {
+    return (
+        <>
+            
+
+            <button 
+            style={{marginBottom: '50px'}}
+            className="option2">LOGIN
+            </button>
+        </>
+    );
+}
+
+export default Noticias;
